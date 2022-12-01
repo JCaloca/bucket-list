8811d55b76f4036d3ea207d09d41a6cea1bb464e
JCaloca <jacobcaloca1@gmail.com>
2022-12-01 14:54:49 -0800

    finished fixing components

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -1,24 +1,27 @@
-import React, { useState } from 'react';
-import BucketForm from './BucketForm';
-import Bucket from './Bucket';
+import React, { useState } from "react";
+import BucketForm from "./BucketForm";
+import Bucket from "./Bucket";
 
 function BucketList() {
   const [bucket, setBucket] = useState([]);
 
   // Function to add a bucket list item
   const addBucketItem = (item) => {
-
     // TODO: Write logic to add the new bucket item to the bucket state variable
-    
+    const newBucket = [item, ...bucket];
+
+    setBucket(newBucket);
   };
 
   // Function to mark bucket list item as complete
   const completeBucketItem = (id) => {
     // If the ID passed to this function matches the ID of the item that was clicked, mark it as complete
     let updatedBucket = bucket.map((item) => {
-      
       // TODO: Write logic that marks an item as complete or incomplete when invoked
-
+      if (id === item.id) {
+        item.completed = true;
+      }
+      return item;
     });
 
     setBucket(updatedBucket);
@@ -27,9 +30,9 @@ function BucketList() {
   // Function to remove bucket list item and update state
   const removeBucketItem = (id) => {
     // TODO: Write logic that will return an array of items that don't contain the ID passed to this function
-
-
     // TODO: Update the bucket state variable
+    let updatedBucket = bucket.filter((item) => item.id !== id);
+    setBucket(updatedBucket);
   };
 
   // Function to edit the bucket list item
